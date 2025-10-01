e22b2a1c5eb4e9d12728b5dc473e16d996114917
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-10-01 15:41:29 +0530

    Referencetype

--- Referencetype.cs
+++ Referencetype.cs
@@ -1,14 +1,18 @@
 using System;
 
+class Person { 
+   public string Name;
+}
 class Program
 {
     static void Main()
     {
-        int a = 10;
-        int b = a; 
-        b = 20;
+        Person p1 = new Person();
+        p1.Name = "Nivi";
 
-        Console.WriteLine(a);
-        Console.WriteLine(b);
+        Person p2 = p1;
+        p2.Name = "Nivethitha";
+
+        Console.WriteLine(p1.Name);
     }
 }
