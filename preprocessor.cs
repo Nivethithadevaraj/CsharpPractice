9952983728a85a3df1c514a17e6504da8806dc65
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-10-02 13:26:21 +0530

    preprocessor

--- preprocessor.cs
+++ preprocessor.cs
@@ -1,15 +1,41 @@
+#define DEBUG
+#define FEATURE_X
+
 using System;
 
 class Program
 {
-	static void Main()
-	{
+    static void Main()
+    {
+        Console.WriteLine("Program started.");
+
 #if DEBUG
-        Console.WriteLine("Debug build: Detailed logs enabled");
-        Console.WriteLine("Connecting to TEST database...");
+        Console.WriteLine("Debug mode: Extra logging enabled.");
 #else
-		Console.WriteLine("Release build: Optimized version");
-		Console.WriteLine("Connecting to PRODUCTION database...");
+        Console.WriteLine("Release mode: Running fast and optimized.");
 #endif
-	}
+
+#if FEATURE_X
+        Console.WriteLine("Feature X is enabled.");
+#elif FEATURE_Y
+        Console.WriteLine("Feature Y is enabled.");
+#else
+        Console.WriteLine("No features enabled.");
+#endif
+
+        #region ExtraCheck
+        int num = 10;
+#if DEBUG
+        if (num > 5)
+        {
+            Console.WriteLine("DEBUG check: num is greater than 5");
+        }
+#endif
+        #endregion
+
+#warning This is just a warning for demonstration.
+#error Uncomment this line to stop compilation with an error!
+
+        Console.WriteLine("Program finished.");
+    }
 }
