0256b1aab4d29915c4f8599039220833881506b1
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-10-04 08:49:42 +0530

    Method overriding

---
+++ method_overriding.cs
@@ -0,0 +1,22 @@
+using System;
+class Animal{
+    public virtual void Speak(){
+        Console.WriteLine("The animal makes a sound");
+    }}
+class Dog:Animal{
+    public override void Speak(){
+        Console.WriteLine("The dog barks");
+    }}
+class Cat:Animal{
+    public override void Speak(){
+        Console.WriteLine("The cat meows");
+    }}
+class Program{
+    static void Main(){
+        Animal myAnimal=new Animal();
+        Animal myDog=new Dog();
+        Animal myCat=new Cat();
+        myAnimal.Speak();
+        myDog.Speak();
+        myCat.Speak();
+    }}
