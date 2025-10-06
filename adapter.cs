09d235ebf10772fc6b082c8f76b46c73eb6adb3b
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-10-06 09:55:13 +0530

    Adapter design pattern

--- adapter.cs
+++ adapter.cs
@@ -1,6 +1,4 @@
 using System;
-
-// Old payment system (already exists)
 public class OldPaymentSystem
 {
     public void MakePayment(double amount)
@@ -8,14 +6,10 @@ public class OldPaymentSystem
         Console.WriteLine($"Payment of {amount} processed using Old System.");
     }
 }
-
-// New interface expected by your application
 public interface INewPayment
 {
     void Pay(double amount);
 }
-
-// Adapter bridges the old system with new interface
 public class PaymentAdapter : INewPayment
 {
     private readonly OldPaymentSystem _oldPayment;
@@ -24,19 +18,16 @@ public class PaymentAdapter : INewPayment
     {
         _oldPayment = oldPayment;
     }
-
     public void Pay(double amount)
     {
-        _oldPayment.MakePayment(amount); // Reusing old method
+        _oldPayment.MakePayment(amount); 
     }
 }
-
-// Client
 class Program
 {
     static void Main()
     {
         INewPayment payment = new PaymentAdapter(new OldPaymentSystem());
-        payment.Pay(500); // ? Works with new interface
+        payment.Pay(500);
     }
 }
